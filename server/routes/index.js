84c19bf1ff5d61861ce5378be17ca497eb6578ec
saltire sable <saltiresable@gmail.com>
2018-07-08 15:50:43 -0400

    Replace async/await with promises for better error handling

--- server/routes/index.js
+++ server/routes/index.js
@@ -9,35 +9,42 @@ const { dataUrl } = require('../utils');
 
 const router = module.exports = express.Router();
 
-router.get('/sword.png', async (req, res) => {
-  const { image } = await swordgen.createRandomSword();
-  res.set('Content-type', 'image/png');
-  res.send(await image.png().toBuffer());
+router.get('/sword.png', (req, res, next) => {
+  swordgen.createRandomSword()
+    .then(({ image }) => image.png().toBuffer())
+    .then((buffer) => {
+      res.set('Content-type', 'image/png');
+      res.send(buffer);
+    })
+    .catch(next);
 });
 
-router.get('/sword/data', async (req, res) => {
-  const { image, text } = await swordgen.createRandomSword();
-
-  res.json({
-    image: await dataUrl(image),
-    desc: text,
-  });
+router.get('/sword/data', (req, res, next) => {
+  swordgen.createRandomSword()
+    .then(({ image, text }) => dataUrl(image)
+      .then(imageUrl => res.json({
+        image: imageUrl,
+        desc: text,
+      })))
+    .catch(next);
 });
 
-router.get('/sword/options', async (req, res) => {
-  const optionSets = await swordgen.selectRandomPaletteOptions();
-  req.session.optionSets = optionSets;
-
-  return res.json({
-    optionSets: optionSets
-      .map(({ palettes }) => Object.values(palettes)
-        .map(({ materials }) => Object.values(materials)
-          .map(mat => mat.replace('*', ''))
-          .join(' and '))),
-  });
+router.get('/sword/options', (req, res, next) => {
+  swordgen.selectRandomPaletteOptions()
+    .then((optionSets) => {
+      req.session.optionSets = optionSets;
+      res.json({
+        optionSets: optionSets
+          .map(({ palettes }) => Object.values(palettes)
+            .map(({ materials }) => Object.values(materials)
+              .map(mat => mat.replace('*', ''))
+              .join(' and '))),
+      });
+    })
+    .catch(next);
 });
 
-router.post('/sword/forge', async (req, res, next) => {
+router.post('/sword/forge', (req, res, next) => {
   const { optionSets } = req.session;
   if (!optionSets) {
     return next(new Error('Session not found.'));
@@ -49,11 +56,13 @@ router.post('/sword/forge', async (req, res, next) => {
     return next(new Error('Choices not found.'));
   }
 
-  const { image, text } = await swordgen.createSwordFromChoices(optionSets, choices);
-  return res.json({
-    image: await dataUrl(image),
-    desc: text,
-  });
+  return swordgen.createSwordFromChoices(optionSets, choices)
+    .then(({ image, text }) => dataUrl(image)
+      .then(imageUrl => res.json({
+        image: imageUrl,
+        desc: text,
+      })))
+    .catch(next);
 });
 
 router.use(app);

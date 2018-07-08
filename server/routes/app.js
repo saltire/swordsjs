fd4d824686a8dcbcbc265e3b88fd71d98e196c6e
saltire sable <saltiresable@gmail.com>
2018-07-08 16:57:48 -0400

    Fix dist path, upgrade package

--- server/routes/app.js
+++ server/routes/app.js
@@ -14,12 +14,12 @@ const router = module.exports = express.Router();
 
 const { publicPath } = webpackConfig.output;
 
-// Rewrite all routes under /app/* (without a dot in the filename) to go to /app/
+// Rewrite all routes under /* (without a dot in the filename) to go to /
 router.use(publicPath, history({ index: '/' }));
 
 if (process.env.NODE_ENV === 'production') {
   // Serve already-compiled webpack content from the dist folder.
-  router.use(publicPath, express.static(path.resolve(__dirname, '../dist')));
+  router.use(publicPath, express.static(path.resolve(__dirname, '../../dist')));
 }
 else {
   const compiler = webpack(webpackConfig);

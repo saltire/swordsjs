6e6b45805294f938eaa67a2159b81fc6ca18e845
saltire sable <saltiresable@gmail.com>
2018-07-15 14:05:31 -0400

    Add a very basic game cycle

--- app/Story.jsx
+++ app/Story.jsx
@@ -1,28 +1,43 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+import './Story.scss';
+
 
 export default class Story extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      state: null,
+      text: null,
     };
+
+    this.continue = this.continue.bind(this);
   }
 
   componentDidMount() {
     axios.get('/story/state')
-      .then(resp => this.setState({ state: resp.data.state }))
+      .then(resp => this.setState({
+        text: resp.data.text,
+      }))
+      .catch(console.error);
+  }
+
+  continue() {
+    axios.post('/story/continue')
+      .then(resp => this.setState({
+        text: resp.data.text,
+      }))
       .catch(console.error);
   }
 
   render() {
-    const { state } = this.state;
+    const { text } = this.state;
 
     return (
       <div className='Story'>
-        Story {JSON.stringify(state)}
+        <p>{text}</p>
+        <button type='button' onClick={this.continue}>Continue</button>
       </div>
     );
   }
--- server/routes/story.js
+++ server/routes/story.js
@@ -2,12 +2,54 @@
 
 const express = require('express');
 
-// const swordgen = require('../swordgen');
-// const { dataUrl } = require('../utils');
+const { random } = require('../lib/utils');
 
+
+const stages = {
+  request: 'A customer enters the shop. “I need a sword that $effect,” they say.',
+  materials: 'You gather materials from the back of the shop, narrowing them down to a couple of choices.',
+  forge: 'You shape the metal into a blade, and decorate it with gems.',
+  deliver: 'The customer takes the blade, says “Thank you,” and leaves.',
+};
+const stageIds = Object.keys(stages);
+
+const effects = [
+  'shoots fire',
+  'glows in the dark',
+  'makes me look rich',
+  'eats the souls of my victims',
+];
+
+function nextStage(currentStory) {
+  const story = Object.assign({}, currentStory || {});
+
+  // Advance to the next stage, or go back to the first if finished.
+  story.stage = stageIds[(stageIds.indexOf(story.stage) + 1) % stageIds.length];
+
+  // Build text for this stage.
+  story.text = stages[story.stage];
+  if (story.stage === 'request') {
+    story.text = story.text.replace('$effect', random(effects));
+  }
+
+  return story;
+}
 
 const router = module.exports = express.Router();
 
 router.get('/state', (req, res) => {
-  res.json({ state: {} });
+  if (!req.session.story) {
+    req.session.story = nextStage();
+  }
+
+  const { story } = req.session;
+
+  res.json({ text: story.text });
+});
+
+router.post('/continue', (req, res) => {
+  const story = nextStage(req.session.story);
+  req.session.story = story;
+
+  res.json({ text: story.text });
 });
